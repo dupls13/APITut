3a7686050db06c96bc7e88103de773d1985a557a
Trevor <shawtrevor97@yahoo.com>
2022-09-25 20:52:27 -0700

    allowed only user to delete/update own post

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -76,21 +76,28 @@ def delete_post(id: int, db:Session = Depends(get_db), current_user: int = Depen
     #conn.commit()
     
     # Grabs query 
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query  = db.query(models.Post).filter(models.Post.id == id)
     
-    if post.first() == None:
+    post = post_query.first()
+    
+    if post_query.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Post with id {id} does not exist')
     
+    # Check if post is owners 
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized for action.")
+    
     #deletes post 
-    post.delete(synchronize_session=False)
+    post_query.delete(synchronize_session=False)
     db.commit()
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db:Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, updated_post: schemas.PostCreate, db:Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
  #   cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
   #                 (post.title, post.content, post.published, str(id),))
     
@@ -109,6 +116,9 @@ def update_post(id: int, updated_post: schemas.PostCreate, db:Session = Depends(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Post with id {id} does not exist')
 
+    if post.owner_id != current_user.id:
+        raise HTTPException(status.HTTP_403_FORBIDDEN,
+                            detail='Not authorized to perform requested action.')
     #Update query. can hard code title, content 
     #post_query.update({'title': 'hey this is my updated title',
                     #   'content': 'this is my updated content'}, synchronize_session=False)
